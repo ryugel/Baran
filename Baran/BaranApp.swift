28f9a61a31f840df8e4df1ad45844837eb1f5238
Augustin Diabira <augustindiabira@gmail.com>
2024-05-03 19:04:50 +0200

    Initial Commit

---
+++ Baran/BaranApp.swift
@@ -0,0 +1,19 @@
+//
+//  BaranApp.swift
+//  Baran
+//
+//  Created by ryugel on 03/05/2024.
+//  Copyright © 2024 DeRosa. All rights reserved.
+//
+       
+
+import SwiftUI
+
+@main
+struct BaranApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Baran/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Baran
+//
+//  Created by ryugel on 03/05/2024.
+//  Copyright © 2024 DeRosa. All rights reserved.
+//
+       
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
